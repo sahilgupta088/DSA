878b57f36d9ffdbcc1c4e6df3db1b0e15d3445b5
Sahil Gupta <154824530+sahilgupta088@users.noreply.github.com>
2026-06-22 13:51:29 +0530

    Time: 1 ms (24.33%) | Memory: 9.1 MB (39.61%) - LeetSync

---
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxNumberOfBalloons(string text) {
+        unordered_map<char,int>mp;
+        for(auto it:text){
+            if (it == 'b' || it == 'a' || it == 'l' || it == 'o' || it == 'n') {
+                mp[it]++;
+            }
+        }
+        int b = mp['b'];
+        int a = mp['a'];
+        int l = mp['l'] / 2;
+        int o = mp['o'] / 2;
+        int n = mp['n'];
+        return min({b,a,l,o,n});
+    }
+};
